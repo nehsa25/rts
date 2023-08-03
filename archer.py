452c5c0cda2dc843a2efe1cc841cde4b4eeea59d
nehsa25 <jesse.stone@nehsa.net>
2023-08-03 14:11:47 -0700

    Update archer.py

--- archer.py
+++ archer.py
@@ -7,7 +7,7 @@ class Archer(Unit):
     combat_range = 2 # 2 tiles
     combat_damage_low = 3
     combat_damage_high = 6
-    attack_tiles = [Environment.levels.Subterranean, Environment.levels.Water, Environment.levels.Ground, Environment.levels.Air]
+    attack_tiles = [Environment.levels.Water, Environment.levels.Ground, Environment.levels.Air]
     move_tiles = [Environment.levels.Ground]
 
     def __init__(self):
