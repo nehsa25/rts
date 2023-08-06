d0af2ddd595715fd217b5a305679f9f42f3f91c4
Crossen295 <ethan@nehsa.net>
2023-08-06 15:16:29 -0700

    Added names

--- names.py
+++ names.py
@@ -3,16 +3,23 @@ import random
 class Names:
     def generate_name(self):
         names = []
-        names.append("Jake")
-        names.append("God")
-        names.append("Kvothe")
+        names.append("Ley")
+        names.append("Sirius")
+        names.append("Capella")
+        names.append("Regulus")
+        names.append("Stride")
+        names.append("Betelgeuse")
+        names.append("Holo")
         name_choice = random.randint(0, len(names) -1)
 
         # title list
         titles = []
         titles.append("the Brave")
+        titles.append("the Cowardly")
         titles.append("the Fool")
-        titles.append("the Strong")
+        titles.append("the greedy")
+        titles.append("the Prideful")
+        titles.append("the Wise")
         title_choice = random.randint(0, len(titles) -1)
 
         # combine name and title
